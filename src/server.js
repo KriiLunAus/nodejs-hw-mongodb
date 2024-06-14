f6a631632e6d61f673090ac67535b9f7764bac81
Krii <kriilunaus16@gmail.com>
2024-06-14 12:41:24 +0200

    start

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+
+
+export default function setupServer() {
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+
+}
